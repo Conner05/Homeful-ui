a1af9fd8bfd8a39dccab9c45a3949bcb4c288d01
Stephen Bussard <stephen.bussard@daveramsey.com>
2017-03-11 17:00:40 -0600

    satisfaction

--- src/app.jsx
+++ src/app.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import {render} from 'react-dom';
 import {Provider} from 'react-redux';
-import {Router, Route, IndexRoute, hashHistory} from 'react-router';
+import {Router, Route, hashHistory} from 'react-router';
 import configureStore from '~/state/configureStore';
 import HomePage from '~/components/HomePage';
 import CampList from '~/components/CampList';
@@ -10,11 +10,13 @@ import '~/theme/base.scss';
 let appContainer = document.querySelector('.App');
 let globalStore = configureStore();
 
-render(
+let Root = () => (
   <Provider store={globalStore}>
     <Router history={hashHistory}>
       <Route path="/" component={HomePage} />
       <Route path="/camp-list" component={CampList} />
     </Router>
   </Provider>
-, appContainer);
+);
+
+render(<Root/>, appContainer);
